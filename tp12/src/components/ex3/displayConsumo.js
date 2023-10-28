90ba2519d47a82e60fa403408a9734c68211fe3f
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-10-28 15:00:46 +0100

    Add tp12 ex 3

--- tp12/src/components/ex3/displayConsumo.js
+++ tp12/src/components/ex3/displayConsumo.js
@@ -3,17 +3,12 @@ import ConsumoForm from './consumoForm';
 import ConsumoChart from './consumoChart';
 
 function DisplayConsumo() {
-  const [consumos, setConsumos] = useState([]);
-
-  const handleConsumoSubmit = (consumo) => {
-    setConsumos([...consumos, consumo]);
-  };
+  
 
   return (
     <div className="App">
       <h1>Controle de Consumo de Combustível</h1>
-      <ConsumoForm onConsumoSubmit={handleConsumoSubmit} />
-      <ConsumoChart data={consumos} />
+     
     </div>
   );
 }
