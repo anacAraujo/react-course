dc088e56a751fd85bcdb4ed924a7311567a0d0bc
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-03 12:23:19 +0000

    Create component

---
+++ tp14/src/components/ex3/receitas.js
@@ -0,0 +1,48 @@
+class Receitas {
+    constructor() {
+        this.data = [
+            {
+                "nome": "Cogumelos Salteados",
+                "ingredientes": [
+                    { "nome": "Cogumelos", "qtd": 1, "medida": "kg" },
+                    { "nome": "Alho", "qtd": 1, "medida": "dentes" },
+                    { "nome": "Azeite", "qtd": 2, "medida": "colher sopa" },
+                    { "nome": "Vinagre Balsamico", "qtd": 1, "medida": "colher cha" }
+
+                ],
+                "passos": [
+                    "Coloque a frigideira no fogo.",
+                    "Coloque o azeite.",
+                    "Adicione o alho picado aos bocados pequenos.",
+                    "Deixe alourar.",
+                    "Adicione os cogumelos.",
+                    "Deixe apurar até ficar com o molho espesso."
+                ]
+            },
+            {
+                "nome": "Moelas",
+                "ingredientes": [
+                    { "nome": "Moelas", "qtd": 1, "medida": "Kg" },
+                    { "nome": "Tomate", "qtd": 2, "medida": "Unidade" },
+                    { "name": "Pimentos", "qtd": 1, "medida": "UNidade" },
+                    { "nome": "Cenoura", "qtd": 1, "medida": "Grande" },
+                    { "name": "Cebola", "qtd": 1, "medida": "Grande" },
+                    { "nome": "Vinho", "qtd": 1, "medida": "Copo" },
+                    { "nome": "Azeite", "qtd": 3, "medida": "Colher" }
+                ],
+                "passos": [
+                    "Faça o refugado com a cebola o azeite e o alho e o pimento.",
+                    "Junte a cenoura.",
+                    "Deixe refugar e junte o vinho.",
+                    "Adicione as moelas .",
+                    "Deixe cozer por 1 hora e meia.",
+                    "Sirva com batatas fritas."
+                ]
+            }
+
+        ];
+    }
+
+}
+
+export default Receitas; 
