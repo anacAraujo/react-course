ae7c9cf92539ca40653a9b899fd40f0ad40c1b06
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-10 12:28:42 +0000

    Create file

---
+++ tp15/src/components/userItem.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+import './styles.css';
+class UserItem extends Component {
+
+	constructor(props) {
+		super(props);
+		this.nome = props.nome;
+		this.username = props.username;
+		this.gn = props.gn;
+		this.tlf = props.tlf;
+	}
+
+	render() {
+		return (
+			<li className="list-group-item">
+				<div className='card'>
+					<h4><b>{this.nome}</b></h4>
+					<p>Username: {this.username}</p>
+					<p>Gender: {this.gn}</p>
+					<p>Phone: {this.tlf}</p>
+				</div>
+			</li>
+		);
+	}
+}
+export default UserItem;
+
+
+
+
+
+
