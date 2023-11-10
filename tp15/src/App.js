9dfbc9b5c83ae6593748dafdccde9997ca6b8ac6
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-10 12:27:59 +0000

    Fix code

--- tp15/src/App.js
+++ tp15/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchForm from './components/search';
 
 function App() {
+
+ // const usersData = new Database();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchForm></SearchForm>
     </div>
   );
 }
