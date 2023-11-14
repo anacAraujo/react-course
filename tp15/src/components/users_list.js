1a900df10564cfcfc293cb26c3bb79495bdcc2fe
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-14 22:56:18 +0000

    Simplify code

--- tp15/src/components/users_list.js
+++ tp15/src/components/users_list.js
@@ -1,30 +1,29 @@
-/**
- * Created by silva1t_admin on 24/10/2017.
- */
 import React from 'react';
 import UserItem from './userItem';
 
 const UsersList = (props) => {
 
 	const usersInfo = props.usersData;
-	console.log(usersInfo);
+	console.log(`usersInfo: ${JSON.stringify(usersInfo)}`);
 
 	// const displayUsersInfoMap = usersInfo.map((user, idx) =>
 	// 	<UserItem key={user.id} nome={user.nome} username={user.username} gn={user.gn} tlf={user.tlf}></UserItem>
 	// );
 
-	function displayUsersInfoFor() {
-		const components = [];
-		for (const user of usersInfo) {
-			const comp = <UserItem key={user.id} nome={user.nome} username={user.username} gn={user.gn} tlf={user.tlf}></UserItem>
-			components.push(comp);
-		}
-		return components;
-	}
+	// function displayUsersInfoFor() {
+	// 	const components = [];
+	// 	for (const user of usersInfo) {
+	// 		const comp = <UserItem key={user.id} nome={user.nome} username={user.username} gn={user.gn} tlf={user.tlf}></UserItem>
+	// 		components.push(comp);
+	// 	}
+	// 	return components;
+	// }
 
 	return (
 		<ul className="col-md-4 list-group">
-			{displayUsersInfoFor()}
+			{usersInfo.map((user) =>
+				<UserItem key={user.id} nome={user.nome} username={user.username} gn={user.gn} tlf={user.tlf}></UserItem>
+			)}
 		</ul>
 	);
 };
