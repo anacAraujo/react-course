40fcd40af8ce6bd3cbb628d10f6d3abdda51d8b4
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-11-15 12:38:24 +0000

    Remove file

--- tp17/src/index.js
+++ tp17/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MyRoute from './components/routes';
 
@@ -10,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MyRoute></MyRoute>
-    <App />
   </React.StrictMode>
 );
 
